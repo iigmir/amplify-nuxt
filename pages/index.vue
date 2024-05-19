a0a45885e5080868aa8ff1d05548fddeb785a2fd
iigmir <roc120j@gmail.com>
2024-05-19 17:24:55 +0800

    Set links

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
     <article>
-        <!-- <NuxtWelcome /> -->
         <h1>Hello World</h1>
-        <small>Where do you want to go today?</small>
+        <small>
+            <a href="https://en.wikipedia.org/wiki/Where_do_you_want_to_go_today%3F">Where do you want to go today?</a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="/bread">bread</a></li>
+                <li><a href="/cat">cat</a></li>
+                <li><a href="/nuxt">nuxt</a></li>
+            </ul>
+        </nav>
     </article>
 </template>
   
